873b740498f0dedadf49a7adfa46fcc797eb330a
gn1m1ew <weimi@oregonstate.edu>
2021-03-07 18:43:38 -0800

    test general case {all test passed}

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -27,13 +27,6 @@ class TestCase(unittest.TestCase):
         self.assertEqual(fizzbuzz.fizzbuzz(30), "FizzBuzz")
     def testfizzbuzz3(self):
         self.assertEqual(fizzbuzz.fizzbuzz(60), "FizzBuzz")
-
-    def testinteger1(self):
-        self.assertEqual(fizzbuzz.fizzbuzz(2), 2)
-    def testinteger2(self):
-        self.assertEqual(fizzbuzz.fizzbuzz(37), 37)
-    def testinteger3(self):
-        self.assertEqual(fizzbuzz.fizzbuzz(79), 79)
     
 if __name__ == '__main__':
     unittest.main(verbosity=2)
